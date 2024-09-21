3cb7c1927f894a77b65bdf44c5eed1768c29c7d5
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-09-21 00:08:21 -0500

    Finished javascript for balance calc

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,14 @@ import {ref, computed} from 'vue'
 const transactions = ref([
   {id: 1, text: 'Paycheck', amount: 700.00},
   {id: 2, text: 'Water Bill', amount: -72.83},
-  {id: 3, text: 'Electric Bill', amount: 153.89},
-  {id: 4, text: 'Return', amount: 20.00}
+  {id: 3, text: 'Electric Bill', amount: -153.89},
+  {id: 4, text: 'Return', amount: 20.00},
 ])
 
 const sum = computed(() => {
   return transactions.value.reduce((acc, x) => {
-
-  })
+      return acc+x.amount
+  },0)
 })
 </script>
 
@@ -22,7 +22,7 @@ const sum = computed(() => {
   <Header></Header>
 
   <div class ="container">
-      <Balance></Balance>
+      <Balance :total="sum"></Balance>
     </div>
 
 </template>
